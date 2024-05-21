2696c1623d904539dc1374f14d618d066fe3b9e1
Belén Robustillo <dev_bec3@dencantocommunity.com>
2024-05-21 12:54:42 +0200

    js change

--- modeltranslation/static/js/modeltranslation/forms/common.js
+++ modeltranslation/static/js/modeltranslation/forms/common.js
@@ -10,15 +10,17 @@ $(document).ready(function(){
 		.prependTo($(".controls:first"))
 		.click(function(e){
 			var source_text = $.trim($("#source_text").html());
+			window.alert('clicked');
+
 			console.log(source_text);
-			// If there is no a TinyMCE editor we have to set its value
-			if($("#id_translation_parent").length==0){
-				$("#id_translation").html(source_text);
-			}
-			// If there is a TinyMCE editor, use its API
-			else{
-				tinymce.activeEditor.setContent(source_text, {format: 'raw'});
-			}
+			tinymce.init({
+                selector: 'textarea'
+                setup: function (editor) {
+                    editor.on('init', function (e) {
+                        editor.setContent(source_text)
+                    });
+                }
+            });
 			return false;
 		});
 
