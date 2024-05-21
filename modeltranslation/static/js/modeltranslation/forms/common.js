3db8f3553180b37561790a90a8b19a7836289d1a
Belén Robustillo <dev_bec3@dencantocommunity.com>
2024-05-21 12:18:00 +0200

    js change

--- modeltranslation/static/js/modeltranslation/forms/common.js
+++ modeltranslation/static/js/modeltranslation/forms/common.js
@@ -4,9 +4,17 @@ $(document).ready(function(){
 		.click(function(e){
 			var source_text = $.trim($("#source_text").html());
 				$("#id_translation").html(source_text);
+			}
+			// If there is a TinyMCE editor, use its API
+			else{
+				tinymce.activeEditor.setContent(source_text, {format: 'raw'});
+				console.debug(tinymce.activeEditor.getContent());
+				tinymce.activeEditor.getContent({ format: 'text' });
+			}
+			tinymce.init({
+                selector: $("<a id='copy_source' class='copy_source' href='javascript:void(0);'>Copy from original text</a>");
+            });
+			return false;
 		});
 
-    tinymce.init({
-      selector: 'textarea'
-    });
 });
