e1b5255c0dd46096aec493b723a8b9f62814cf52
Belén Robustillo <dev_bec3@dencantocommunity.com>
2024-05-21 12:32:23 +0200

    js change

--- modeltranslation/static/js/modeltranslation/forms/common.js
+++ modeltranslation/static/js/modeltranslation/forms/common.js
@@ -3,7 +3,12 @@ $(document).ready(function(){
 		.prependTo($(".controls:first"))
 		.click(function(e){
 			var source_text = $.trim($("#source_text").html());
-            $("#id_translation").html(source_text);
+				$("#id_translation").html(source_text);
+			}
+			// If there is a TinyMCE editor, use its API
+			else{
+				tinymce.activeEditor.setContent(source_text);
+			}
 
 			return false;
 		});
